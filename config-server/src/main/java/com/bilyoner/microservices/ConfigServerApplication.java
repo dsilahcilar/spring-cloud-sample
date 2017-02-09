9d2cb4c73c649bb14048559c79b1a4c4e0299637
Ogün Heper <ogun.heper@gmail.com>
2017-02-09 17:28:03 +0300

    Initial config-server integration

--- config-server/src/main/java/com/bilyoner/microservices/ConfigServerApplication.java
+++ config-server/src/main/java/com/bilyoner/microservices/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.bilyoner.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
