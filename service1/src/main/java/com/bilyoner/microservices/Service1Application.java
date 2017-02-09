b6b089deed89f70db440d9d2ae6d5ed2d08c36db
Deniz Silahcılar <dsilahcilar@gmail.com>
2017-02-09 13:16:16 +0300

    Service Discovery initial

--- service1/src/main/java/com/bilyoner/microservices/Service1Application.java
+++ service1/src/main/java/com/bilyoner/microservices/Service1Application.java
@@ -2,8 +2,10 @@ package com.bilyoner.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Service1Application {
 
     public static void main(String[] args) {
